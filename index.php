328a5e1f3aee3033a287a147b85264ef2822ee2a
LIefr1 <cstud129306@gmail.com>
2023-10-04 20:04:51 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "style.css">  
+
+    <title>Buisness card</title>
+</head>
+<body>
+<div class="home-screen">
+    <div class="photo-socials">
+        <div class="photo">
+            <img src = "/assets/MyPhoto.png">
+        </div>
+        <div class="socials">
+            <div class="whats-app">
+                <input type="image" src="/assets//whatsapp.png" alt="WhatsApp">
+            </div>
+            <div class="telegram">
+                <input type="image" src="/assets//telegram.png" alt="Telegram">
+            </div>
+            <div class="github">
+                <input type="image" src="/assets//github.png" alt="Github">
+            </div>
+            <div class="gmail">
+                <input type="image" src="/assets//gmail.png" alt="Gmail">
+            </div>
+        </div>
+    </div>
+    <div class="aboutMe">
+        <div class="aboutMe-text">
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta tristique finibus. Proin id imperdiet enim. Etiam sapien odio, faucibus eget nibh eget, euismod semper arcu. Integer pulvinar ante non lorem gravida porta. Morbi sed lorem in elit porttitor pretium tempor et nulla. Vestibulum ultrices metus non arcu egestas pulvinar. </p>
+        </div>
+    </div>
+</div>
+
+</div>
+<div class="scroll-down"> </div>
+</body>
+</html>
