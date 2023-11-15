8222301619657ac2ec735e6d0bc7d1db10c8e5f2
LIefr1 <cstud129306@gmail.com>
2023-11-15 06:21:05 +0300

    frontend

--- index.php
+++ index.php
@@ -1,65 +1,58 @@
 <!DOCTYPE html>
 <html>
   <head>
+      <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="globals.css" />
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
     <div class="page">
-      <header class="header"><div class="about-me-text">RESUME</div></header>
+      <header class="header"><div class="header-text">Buisness card</div></header>
       <div class="main">
         <div class="social-photo">
-          <div class="photo"><img class="my-photo" src="img/my-photo.png" /></div>
+          <div class="photo"><img class="my-photo" src="assets/Myphoto.png" /></div>
           <div class="socials">
             <div class="group">
-              <img class="github" src="img/github.png" />
-              <img class="gmail" src="img/gmail.png" />
-              <img class="whatsapp" src="img/whatsapp.png" />
-              <img class="telegram" src="img/telegram.png" />
+              <img class="github" src="assets/github.png" />
+              <img class="gmail" src="assets/gmail.png" />
+              <img class="whatsapp" src="assets/whatsapp.png" />
+              <img class="telegram" src="assets/telegram.png" />
             </div>
           </div>
         </div>
         <div class="about-me">
           <p class="text-wrapper">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta tristique finibus. Proin id imperdiet
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta tristique finibus. Proi n id imperdiet
             enim. Etiam sapien odio, faucibus eget nibh eget, euismod semper arcu. Integer pulvinar ante non lorem
             gravida
           </p>
         </div>
       </div>
       <div class="skills">
-        <div class="previos-work">
-          <div class="overlap-group">
-            <div class="frame"><div class="div">Workplaces</div></div>
-            <p class="work-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta tristique finibus. Proin id imperdiet
-              enim. Etiam sapien odio, faucibus eget nibh eget, euismod semper arcu. Integer pulvinar ante non lorem
-              gravida
-            </p>
+        <div class="skillNames">
+          <div id="previos-work">
+            <p class="skill-title">Workplaces</p>
+          </div>
+          <div id="soft-skills">
+            <p class="skill-title">Soft skills</p>
+          </div>
+          <div id="hard-skills">
+            <p class="skill-title">Hard skills</p>
           </div>
         </div>
-        <div class="soft-sklls">
-          <div class="about-me-text-wrapper"><div class="div">Soft skills</div></div>
-          <p class="soft-text">
+        <div class="skill-text">
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta tristique finibus. Proin id imperdiet
             enim. Etiam sapien odio, faucibus eget nibh eget, euismod semper arcu. Integer pulvinar ante non lorem
             gravida
           </p>
-        </div>
-        <div class="hard-skills">
-          <div class="about-me-text-wrapper"><div class="div">Hard skills</div></div>
-          <p class="soft-s-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta tristique finibus. Proin id imperdiet
-            enim. Etiam sapien odio, faucibus eget nibh eget, euismod semper arcu. Integer pulvinar ante non lorem
-            gravida
-          </p>
-        </div>
+          </div>
       </div>
       <footer class="footer">
         <p class="made-with-flaticon">
           <span class="span">Made with </span>
           <span class="text-wrapper-2">flaticon icons</span>
-          <span class="span">&nbsp;</span>
         </p>
       </footer>
     </div>
